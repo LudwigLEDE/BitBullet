61de1106e4a04c4ef60716aafdac5a736b308584
Assasine03 <clashoderso@gmail.com>
2025-03-24 19:38:55 +0100

    Proper barebones Room connecting for player and room management

---
+++ Assets/Scrips/RoomManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public GameObject player;
+    [Space]
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+
+        Debug.Log("Connect to Room");        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("Joined Room");
+        GameObject plr = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+    }
+}
